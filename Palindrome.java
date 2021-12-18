d29f09119a6d949de0e68e9ac637f3a94dab884c
Vishal Singh <vsing8298@gmail.com>
2021-12-18 11:32:02 +0530

    Adding java Programs

---
+++ Palindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class PalindromeList{
+	private int num=0;
+	private int rev=0;
+	public PalindromeList(int no){
+	
+			this.num=no;
+			while(no!=0){
+				int rem=no%10;
+				this.rev=this.rev*10+rem;
+				no=no/10;
+			}
+		
+	}
+	public void checkPalindrome(){
+		if(this.num>=10){
+			if(num==rev){
+				System.out.println(this.num+" is Palindrome ");
+			}
+		}
+	}
+}
+class Palindrome{
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter lower limit : ");
+		int ll=input.nextInt();
+		System.out.print("Enter upper limit : ");
+		int ul=input.nextInt();
+		for(int i=ll;i<=ul;i++){
+			PalindromeList pl=new PalindromeList(i);
+			pl.checkPalindrome();
+		}
+	}
+}
---
+++ first_day.bat
@@ -0,0 +1,6 @@
+SET PATH=C:\Program Files\Java\jdk-17\bin
+javac palindrome.java
+
+java Palindrome
+
+pause
